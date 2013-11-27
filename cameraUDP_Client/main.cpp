196b11bcc68f25e9f8dc44afc702b69b32182645
Katsuya Fujii <katsuya0515@guest180.csl.sony.co.jp>
2013-11-27 17:51:18 +0900

    Initial Commit

---
+++ cameraUDP_Client/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cameraUDP_Client
+//
+//  Created by Katsuya Fujii on 11/27/13.
+//  Copyright (c) 2013 Katsuya Fujii. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
